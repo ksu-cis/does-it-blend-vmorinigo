5b57241b81b5a5960d79afa25e2b8de141c196c4
vmorinigo <vmorinigo@F1092-23-2111>
2020-02-05 14:15:49 -0600

    Does it Blend

--- DoesItBlend/Program.cs
+++ DoesItBlend/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DoesItBlend
 {
@@ -7,6 +8,23 @@ namespace DoesItBlend
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var b = new Banana();
+            var s = new Strawberry();
+            var m = new Mango();
+            var c = new IceCubes();
+            var p = new CellPhone();
+
+            Console.WriteLine(b.Blend());
+            Console.WriteLine(s.Blend());
+            Console.WriteLine(m.Blend());
+
+            List<IBlendable> fruits = new List<IBlendable>() { b, s, m, c, p};
+            foreach(var f in fruits)
+            {
+                Console.WriteLine(f.GetType());
+                Console.WriteLine(f.Blend());
+            }
         }
     }
 }
